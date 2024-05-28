cdfadfb77c1f6e7cdefd153d3471b38f4e94892d
Aabdulloh2005 <ganievabdulloh2005@gmail.com>
2024-05-28 09:22:31 +0500

    updated

--- lib/views/widgets/company_widgets.dart
+++ lib/views/widgets/company_widgets.dart
@@ -8,18 +8,22 @@ class CompanyWidgets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(
-          title,
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        Expanded(
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
         ),
-        Text(
-          name,
-          style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepPurple),
+        Expanded(
+          flex: 2,
+          child: Text(
+            name,
+            style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple),
+          ),
         ),
       ],
     );
